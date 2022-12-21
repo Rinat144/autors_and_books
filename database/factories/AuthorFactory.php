ef3d1300ea4c003d3fccfaa16a7e91deeedb3bea
Rinat144 <rinatik14@gmail.com>
2022-12-21 13:08:06 +0300

    третий коммит

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Author;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class AuthorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Author::class;
+    public function definition()
+    {
+        $name = $this->faker->name . ' ' . $this->faker->firstName;
+        return [
+            'name' => $name,
+            'date_at' => $this->faker->date,
+            'slug' => Str::slug($name, '-'),
+        ];
+    }
+}
